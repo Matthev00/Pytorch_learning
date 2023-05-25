84aba0c10d3db1edaa92acb835bc555a972fdcb8
Matthev00 <mateuszostaszewskififa@gmail.com>
2023-05-25 11:52:42 +0200

    added parsing arguments to utils

--- going_modular/utils.py
+++ going_modular/utils.py
@@ -1,5 +1,6 @@
 import torch
 from pathlib import Path
+import argparse
 
 
 def save_model(model: torch.nn.Module,
@@ -20,3 +21,25 @@ def set_seeds(seed: int = 42):
     torch.manual_seed(seed)
     # Set the seed for CUDA torch operations (ones that happen on the GPU)
     torch.cuda.manual_seed(seed)
+
+
+def parse_arguments():
+    """
+    Parse arguments:
+    - batch size
+    - hidden units
+    - learning rate as lr
+    - num of epochs
+    """
+    parser = argparse.ArgumentParser(description='Script train model TinyVGG')
+    parser.add_argument('--batch_size', type=int, default=32,
+                        help='Batch size for training and testing')
+    parser.add_argument('--hidden_units', type=int, default=10,
+                        help='Number of hidden units in the model')
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='Learning rate for the optimizer')
+    parser.add_argument('--num_epochs', type=int, default=5,
+                        help='Number of epochs for training')
+
+    args = parser.parse_args()
+    return args
